0abfffd84f4f0583235015481fb6c37c633f194a
ryorangga.suma@gmail.com <ryorangga.suma@gmail.com>
2021-01-12 10:48:25 +0700

    Pull aku

--- contohGit/src/contohGit/Awal.java
+++ contohGit/src/contohGit/Awal.java
@@ -5,6 +5,7 @@ public class Awal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Coba git");
+		System.out.println("ini harus di pull");
 	}
 
 }
